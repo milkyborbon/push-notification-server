124f3b3dff3438551b477fe718ba6e28b859c27c
Glitch (hello-express) <none>
2023-03-07 16:57:14 +0000

    🏰😎 Checkpoint ./server.js:53315271/1715

--- server.js
+++ server.js
@@ -17,6 +17,33 @@ db.defaults({
 
 function sendNotifications(subscriptions) {
   // TODO
+  // Create the notification content.
+  const notification = JSON.stringify({
+    title: "Hello, Notifications!",
+    options: {
+      body: `ID: ${Math.floor(Math.random() * 100)}`
+    }
+  });
+  // Customize how the push service should attempt to deliver the push message.
+  // And provide authentication information.
+  const options = {
+    TTL: 10000,
+    vapidDetails: vapidDetails
+  };
+  // Send a push message to each client specified in the subscriptions array.
+  subscriptions.forEach(subscription => {
+    const endpoint = subscription.endpoint;
+    const id = endpoint.substr((endpoint.length - 8), endpoint.length);
+    webpush.sendNotification(subscription, notification, options)
+      .then(result => {
+        console.log(`Endpoint ID: ${id}`);
+        console.log(`Result: ${result.statusCode}`);
+      })
+      .catch(error => {
+        console.log(`Endpoint ID: ${id}`);
+        console.log(`Error: ${error} `);
+      });
+  });
 }
 
 const app = express();
@@ -32,20 +59,31 @@ app.post('/add-subscription', (request, response) => {
 });
 
 app.post('/remove-subscription', (request, response) => {
-  console.log('/remove-subscription');
-  console.log(request.body);
+  console.log(`Unsubscribing ${request.body.endpoint}`);
+  db.get('subscriptions')
+    .remove({endpoint: request.body.endpoint})
+    .write();
   response.sendStatus(200);
 });
 
 app.post('/notify-me', (request, response) => {
-  console.log('/notify-me');
-  console.log(request.body);
+  console.log(`Notifying ${request.body.endpoint}`);
+  const subscription = 
+      db.get('subscriptions').find({endpoint: request.body.endpoint}).value();
+  sendNotifications([subscription]);
   response.sendStatus(200);
 });
 
 app.post('/notify-all', (request, response) => {
-  console.log('/notify-all');
-  response.sendStatus(200);
+  console.log('Notifying all subscribers');
+  const subscriptions =
+      db.get('subscriptions').cloneDeep().value();
+  if (subscriptions.length > 0) {
+    sendNotifications(subscriptions);
+    response.sendStatus(200);
+  } else {
+    response.sendStatus(409);
+  }
 });
 
 app.get('/', (request, response) => {
